f407451302d4d09a8175124e074e4eb3ec4b6b63
pienath <156034508+pienath@users.noreply.github.com>
2025-07-10 18:00:38 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jul 12, 2025',
+  birthdate: 'Jul 12, 2024',
   name: 'BIBIL!'
 };
 
